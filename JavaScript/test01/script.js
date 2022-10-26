b64beef88a5544603c23351c309f2edd87f8126f
Phfish1 <philip.skudvig@icloud.com>
2022-10-26 02:22:34 +0200

    WEB02

---
+++ JavaScript/test01/script.js
@@ -0,0 +1,11 @@
+/*
+function myFunction() {
+    alert("Hello Philip");
+}
+*/
+
+function myFunction() {
+    document.getElementById("change1").innerHTML = "[ I am The father ]";
+    document.getElementById("change1").id = "test";
+    document.getElementById("bodyid").id = "bodyback";
+}
